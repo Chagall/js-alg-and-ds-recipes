6ce34c2560f17feb7dce2570a9afc0944319f8df
Fábio A. M. Pereira <famp@operamail.com>
2022-03-13 16:15:41 -0300

    Added a javscript implementation of Insertion Sort. As of now has been tested to work with numbers, maybe it works with strings as well (not sure).

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,35 @@
+/*
+    Insert:
+    Parameters:
+        - array: The array to be sorted
+        - index: The index of the element to be sorted
+*/
+function insert(array, index) {
+    for(let i = (index - 1); i >= 0; i--) {
+        if(array[i] > array[i+1]) {
+            console.log("Swapping: ", array[i], " and ", array[i+1]);
+            // Swap
+            let temp = array[i+1];
+            array[i+1] = array[i];
+            array[i] = temp; 
+        }
+        else {
+            break;
+        }
+    }
+}
+
+/*
+    Insertion Sort
+    Parameters: 
+        - array: The array to be sorted
+    Return:
+        - The sorted array
+*/
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+        insert(array,i);
+    }
+
+    return array;
+}
