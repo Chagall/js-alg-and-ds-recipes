caf5ba343cc0f5da5f7595252f7eac27a72be232
Fábio A. M. Pereira <famp@operamail.com>
2022-03-14 01:03:45 -0300

    Changed the ident from 4 to 2 spaces.

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -5,18 +5,17 @@
         - index: The index of the element to be sorted
 */
 function insert(array, index) {
-    for(let i = (index - 1); i >= 0; i--) {
-        if(array[i] > array[i+1]) {
-            console.log("Swapping: ", array[i], " and ", array[i+1]);
-            // Swap
-            let temp = array[i+1];
-            array[i+1] = array[i];
-            array[i] = temp; 
-        }
-        else {
-            break;
-        }
+  for (let i = index - 1; i >= 0; i--) {
+    if (array[i] > array[i + 1]) {
+      console.log("Swapping: ", array[i], " and ", array[i + 1]);
+      // Swap
+      let temp = array[i + 1];
+      array[i + 1] = array[i];
+      array[i] = temp;
+    } else {
+      break;
     }
+  }
 }
 
 /*
@@ -27,9 +26,9 @@ function insert(array, index) {
         - The sorted array
 */
 function insertionSort(array) {
-    for(let i = 1; i < array.length; i++) {
-        insert(array,i);
-    }
+  for (let i = 1; i < array.length; i++) {
+    insert(array, i);
+  }
 
-    return array;
+  return array;
 }
